a4b909f63e8a4cc095d9032745627e72c76d4198
Rio Kuchlyan <riokuchlyan@gmail.com>
2024-09-29 13:10:54 +0100

    Add text output box and button functionality.

--- main.py
+++ main.py
@@ -2,19 +2,33 @@ import tkinter as tk
 
 passwords=[]
 
-def addToPasswordsList(username, note, password):
-    item=[username, note, password]
+def initialization():
+    #read and decrypt password data from csv file and print to textOutput
+    return
+
+def addToPasswordsList():
+    item=[userNameVar.get(), notesVar.get(), passwordVar.get()]
+    for i in passwords:
+        if item == i:
+            return
     passwords.append(item)
+    labeledPasswordEntry="Username: " + userNameVar.get() + " | " + "Note: " + notesVar.get() + " | " + "Password: " + passwordVar.get()
+    textOutput.insert(tk.END, labeledPasswordEntry)
+    textOutput.insert(tk.END, "\n")
+    #add funcitonality to write and encrypt password data to csv file
 
 def job():
     print(passwords)
     window.after(2000, job)
 
+
 #GUI
 window=tk.Tk()
-textOutput = tk.Text(window, height = 500, width = 800)
+textOutput = tk.Text(window, height = 33, width = 127)
 window.title("Password Manager")
 window.geometry("900x500")
+window.resizable(False, False)
+
 
 #variables
 userNameVar=tk.StringVar()
@@ -29,10 +43,10 @@ entryInputNote=tk.Entry(window, textvariable=notesVar)
 entryLabelPassword=tk.Label(window, text="Password: ")
 entryInputPassword=tk.Entry(window, textvariable=passwordVar)
 #button does not work
-addButton=tk.Button(window, text="Add", command=addToPasswordsList(userNameVar.get(), notesVar.get(), passwordVar.get()))
+addButton=tk.Button(window, text="Add", command=addToPasswordsList)
 
 #place tkinter widgets
-#textOutput.pack()
+textOutput.place(x=0,y=0)
 entryLabelUsername.place(x=20, y=450)
 entryInputUsername.place(x=90, y=450)
 entryLabelNote.place(x=300, y=450)
@@ -52,7 +66,9 @@ while True:
 links:
 https://stackoverflow.com/questions/2490334/simple-way-to-encode-a-string-according-to-a-password
 https://www.geeksforgeeks.org/how-to-encrypt-and-decrypt-strings-in-python/
+"""
 
+"""
 functions include:
 generating strong passwords
 hashing and encrypting given passwords
