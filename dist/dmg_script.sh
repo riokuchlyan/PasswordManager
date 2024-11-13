cbb3590bee0b15f215913346ad09012d4feb872b
Rio Kuchlyan <riokuchlyan@gmail.com>
2024-11-13 17:14:39 +0000

    Create dmg file

---
+++ dist/dmg_script.sh
@@ -0,0 +1,11 @@
+create-dmg \
+  --volname "Password Manager" \
+  --volicon "icon.icns" \
+  --window-pos 200 120 \
+  --window-size 600 300 \
+  --icon-size 100 \
+  --icon "Password Manager.app" 175 120 \
+  --hide-extension "Password Manager.app" \
+  --app-drop-link 425 120 \
+  "dist/Password Manager.dmg" \
+  "dist/dmg/"
